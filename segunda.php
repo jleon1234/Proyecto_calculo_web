5d1b88a9c4ec20b3c279541bdd79877aa11e243b
erickgjs99 <erickjs99lol@gmail.com>
2019-07-06 21:21:40 -0500

    agregando el menu unid 2

--- segunda.php
+++ segunda.php
@@ -4,18 +4,55 @@
 
 <div class="all-title-box">
     <div class="container text-center">
-        <h1>Segunda Unidad<span class="m_1">Descripcion corta de la unidad</span></h1>
+        <h1>Segunda Unidad<span class="m_1">Funciones</span></h1>
     </div>
 </div>
 
 <div class="container">
-    <div class="row">
+    <div class="row contenido">
         <!-- menu despegable -->
-        <div class="col-md-6 menu-temas">
-            
+        <div class="col-md-4 menu-temas">
+            <div class = "menu_temas">
+            <ul>
+                <li><a hrfe = "#funciones">1. Funciones</a></li>
+                <ul>
+                    <li><a hrfe = "#concepto1">1.1 Concepto</a></li>    
+                    <li><a hrfe = "#representacion">1.2 Representación</a></li>
+                    <li><a hrfe = "#funcionpoli">1.3 Función polinominal</a></li>
+                    <ul>
+                        <li><a hrfe = "#tipopoli">1.3.1 Tipos de función polinominal</a></li>
+                    </ul>
+                        <li><a hrfe = "#ejemplo1">1.4 Ejemplos</a></li>
+                    </ul>
+                    <li><a hrfe = "#funciones_E">2. Funciones Exponenciales</a></li>
+                    <ul>
+                        <li><a hrfe = "#concepto2">2.1 Concepto</a></li>   
+                        <li><a hrfe = "#prioridades1">2.2 Prioridades en funciones exponenciales</a></li>   
+                        <li><a hrfe = "#ejemplo2">2.3 Ejemplo</a></li>    
+                    </ul>
+                    <li><a hrfe = "#funciones_L">3. Funciones Logarítmicas</a></li>
+                    <ul>
+                        <li><a hrfe = "#concepto3">3.1 Concepto</a></li>   
+                        <li><a hrfe = "#prioridades2">3.2 Prioridades en funciones logarítmicas</a></li>   
+                        <li><a hrfe = "#ejemplo3">3.3 Ejemplo</a></li>    
+                    </ul>
+                </ul>
+            </div>
         </div>
-        <div class="col-md-8">
-            
+         <!-- Contenido -->
+        <div class = "col-md-8">
+            <div class = "contenido_temas">
+                <div id = "funciones">
+                    <h2><b>1. Funciones</b></h2>
+                    <p>
+                    Una variable es un símbolo cualquier que puede representar un valor
+                    </p>
+                </div>
+                <div id = "concepto1">
+                
+                
+                </div>
+            </div>
         </div>
     </div>
 </div>
