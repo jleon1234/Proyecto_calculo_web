34ffc469acf3990c13b5df961c05fcb8b3fa2dcf
davisalex22 <davis.dassr@gmail.com>
2019-07-08 08:06:26 -0500

    Corriegiendo tildes

--- graficar.php
+++ graficar.php
@@ -4,7 +4,7 @@
 
 <div class="all-title-box">
     <div class="container text-center">
-        <h1>Graficar Funcion<span class="m_1">Esta pequeña app te Ayudara a obtener los valores para poder graficar cualquier funcion cuadratica</span></h1>
+        <h1>Graficar Función<span class="m_1">Esta pequeña app te Ayudara a obtener los valores para poder graficar cualquier función cuadrática</span></h1>
     </div>
 </div>
 
@@ -13,7 +13,7 @@
         <!-- menu despegable -->
         <div class="col-md-4">
             <div class="form-group">
-                <label for="">Ingrese la funcion: </label>
+                <label for="">Ingrese la función: </label>
             </div>
             <div class="form-group">
                 <input type="number" id="cuadrado" min="0" pattern="^[0-9]+">X^2<input type="text" id="signo1" placeholder="+"><input type="number" id="uno" placeholder="2" min="0" pattern="^[0-9]+">X<input type="text" id="signo2" placeholder="-" ><input type="number" id="solo" placeholder="10" min="0" pattern="^[0-9]+"> <br>
